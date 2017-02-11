2bce7b3024660d3f25593801e32fb1ad04f71d47
Hamed <hamed@Sherrys-MacBook.lan>
2017-02-11 21:01:52 +0100

    OK

--- insert.js
+++ insert.js
@@ -7,7 +7,20 @@ import stream from 'stream'
 import es from 'event-stream'
 
 const MONGODB_SERVER = 'mongodb://localhost:27017/book'
-const SOURCE_DIRECTORY = './source'
+const TRANSLATIONS_SOURCE_DIRECTORY = './source/translations'
+const LANGUAGE_ABBRIVIATION_LENGTH = 2
+const FILE_NAME_SEPERATOR = '.'
+const ACCEPTED_SEPERATOR = '-'
+const FILE_EXTENSION = '.txt'
+const BOOK_NAME = 'quran'
+
+const METADATA_TEMPLATE = {
+    NAME: '#  Name: ',
+    TRANSLATOR: '#  Translator: ',
+    ID: '#  ID: ',
+    LASTUPDATE: '#  Last Update:',
+    SOURCE: '#  Source: ',
+}
 
 function connect(server) {
     return new Promise((resolve, reject) => {
@@ -35,39 +48,84 @@ async function start(name, {} = {}) {
     try {
         let db = await connect(MONGODB_SERVER)
         debug('connected db:', 'book')
-        let fileList = await read(SOURCE_DIRECTORY)
+        let fileList = await read(TRANSLATIONS_SOURCE_DIRECTORY)
         debug('read dir:', fileList)
-
+        let book = {}
         fileList.map((file) => {
-            let collection = db.collection(file)
-            let lineNr = 0
-            console.log(SOURCE_DIRECTORY + '/' + file)
-            let stream = fs.createReadStream(SOURCE_DIRECTORY + '/' + file)
+
+
+            // Books Metadata
+            book.id = file.split(FILE_EXTENSION)[0].replace(FILE_NAME_SEPERATOR, ACCEPTED_SEPERATOR)
+            book.language = file.split(FILE_NAME_SEPERATOR)[0]
+            book.translator = file.split(FILE_NAME_SEPERATOR)[1].split(FILE_EXTENSION)[0]
+            book.name = BOOK_NAME
+
+            info(book)
+
+            let metadata = {}
+            let collection = db.collection(book.id)
+            // collection.insert({metadata: {}, context: []})
+            let lineNumber = 0
+            let stream = fs.createReadStream(TRANSLATIONS_SOURCE_DIRECTORY + '/' + file)
                 .pipe(es.split())
                 .pipe(es.mapSync(function (line) {
                     stream.pause()
-
-                    lineNr += 1
-
-                    // process line here and call s.resume() when rdy
-                    // function below was for logging memory usage
-                    debug(lineNr + ': ' + line)
-                    
-                    let doc = {
-                        index: lineNr
+                    lineNumber += 1
+                    let firstChar = line.slice(0, 1)
+                    if (firstChar === '#') {
+                        if (line.match(METADATA_TEMPLATE.NAME)) metadata.name = line.split(METADATA_TEMPLATE.NAME)[1]
+                        if (line.match(METADATA_TEMPLATE.TRANSLATOR)) metadata.translator = line.split(METADATA_TEMPLATE.TRANSLATOR)[1]
+                        if (line.match(METADATA_TEMPLATE.ID)) metadata.id = line.split(METADATA_TEMPLATE.ID)[1]
+                        if (line.match(METADATA_TEMPLATE.LASTUPDATE)) metadata.lastUpdate = line.split(METADATA_TEMPLATE.LASTUPDATE)[1]
+                        if (line.match(METADATA_TEMPLATE.SOURCE)) metadata.source = line.split(METADATA_TEMPLATE.SOURCE)[1]
+                    } else if (firstChar === ' ' || firstChar === '\n') {
+                        // Ignore lines start wich character: space
+                    } else {
+                        // let doc = {
+                        //     index: lineNumber
+                        // }
+                        // doc[book.id] = line
+                        // collection.insert(doc)
+                        // debug(lineNumber + ':', doc)
                     }
-                    
-                    doc[file.replace('.', '-').split('.txt')] = line
-                    collection.insert(doc)
-
-                    // resume the readstream, possibly from a callback
                     stream.resume()
                 })
                     .on('error', function (err) {
                         console.log('Error while reading file.', err)
                     })
-                    .on('end', function (err) {
-                        console.log('Read entire file.', err)
+                    .on('end', function () {
+                        let stream2 = fs.createReadStream(TRANSLATIONS_SOURCE_DIRECTORY + '/' + file)
+                            .pipe(es.split())
+                            .pipe(es.mapSync(function (line) {
+                                stream2.pause()
+                                lineNumber += 1
+                                let firstChar = line.slice(0, 1)
+                                if (firstChar === '#') {
+                                    // if (line.match(METADATA_TEMPLATE.NAME)) metadata.name = line.split(METADATA_TEMPLATE.NAME)[1]
+                                    // if (line.match(METADATA_TEMPLATE.TRANSLATOR)) metadata.translator = line.split(METADATA_TEMPLATE.TRANSLATOR)[1]
+                                    // if (line.match(METADATA_TEMPLATE.ID)) metadata.id = line.split(METADATA_TEMPLATE.ID)[1]
+                                    // if (line.match(METADATA_TEMPLATE.LASTUPDATE)) metadata.lastUpdate = line.split(METADATA_TEMPLATE.LASTUPDATE)[1]
+                                    // if (line.match(METADATA_TEMPLATE.SOURCE)) metadata.source = line.split(METADATA_TEMPLATE.SOURCE)[1]
+                                } else if (firstChar === ' ' || firstChar === '\n') {
+                                    // Ignore lines start wich character: space
+                                } else {
+                                    let doc = {
+                                        index: lineNumber
+                                    }
+                                    doc[book.id] = line
+                                    doc['translator'] = metadata.translator
+                                    collection.insert(doc)
+                                    debug(lineNumber + ':', doc)
+                                }
+                                stream2.resume()
+                            })
+                                .on('error', function (err) {
+                                    console.log('Error while reading file.', err)
+                                })
+                                .on('end', function () {
+                                    info(metadata)
+                                })
+                            )
                     })
                 )
         })
